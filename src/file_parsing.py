c3b0ee905f0a412598f3cc2b3f991732649667ad
Sahas_Sangal <sangal.s001@gmail.com>
2024-09-06 11:48:15 +0530

    drawing out required graphs

--- src/file_parsing.py
+++ src/file_parsing.py
@@ -1,12 +1,13 @@
 from cache import *
 
 
-def read_trace_file(file_path):
+def read_trace_file(file_path,cache_size = 1024,associativity = 4,block_size = 4):
     # count = 0
-    obj = Cache(4096, 4, 4)
+    # obj = Cache(4096, 4, 4)
+    obj = Cache(cache_size,associativity, block_size)
     with open(file_path, "r") as file:
         for line in file:
             address = str(line.strip().split(" ")[1])
             obj.mem_access(address)
 
-    print(obj.hit_rate(), " ", obj.miss_rate())
+    return obj.hit_rate()
